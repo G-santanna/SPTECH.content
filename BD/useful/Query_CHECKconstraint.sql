87e48b03a94e2ef6164f76aca3730fd66a1a7c6d
G-santanna <gustavo.soliveira@sptech.school>
2023-08-31 15:41:12 -0300

    working

--- BD/useful/Query_CHECKconstraint.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE revista ADD CONSTRAINT chkCategoria
-	CHECK (categoria IN ('Diversos', 'Infanto-juvenil'));
-    
--- ALTER TABLE tabela ADD CONSTRAINT nomeRegra
------ CHECK (colunaRegra IN ('valor1', 'valor2'));
