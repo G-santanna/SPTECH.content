0870fc38321e020db60568a47866760b3a10d755
G-santanna <gustavo.soliveira@sptech.school>
2023-09-28 09:12:49 -0300

    Atividades de banco e algoritimos(var global)

---
+++ BD/useful/QueryDCL.sql
@@ -0,0 +1,13 @@
+create user 'supervisor'@'localhost' identified by '#gf202302';
+
+grant SELECT, UPDATE, DELETE, INSERT on sptechb.* to 'supervisor'@'localhost';
+
+flush privileges;
+
+revoke SELECT, UPDATE, DELETE, INSERT on sptechb.* from 'supervisor'@'localhost';
+
+flush privileges;
+
+grant all privileges on sptechb.* to 'supervisor'@'localhost';
+
+flush privileges;
